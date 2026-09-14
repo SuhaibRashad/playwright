af8476148fce52098a723242a9c5d2e256ce4859
SuhaibRashad <mdsuhaibengineer@gmail.com>
2026-09-14 09:53:59 +0530

    L15 css selectors

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,14 +1,20 @@
 const {test} = require("@playwright/test")
 
-    test(
+    test.only(
         "Browser Context playwright test",
         async ({browser}) => {
             const context = await browser.newContext();
             const page = await context.newPage();
-            page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+            await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+            await page.locator("#username").fill("rahulshetty"); // id selector
+            await page.locator("[type='password']").fill("learning"); // attribute selector
+            await page.locator("#signInBtn").click()
+
+
         }
     )
 
-        test("Page (default ) playwright test", async ({ page }) => {
-          page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-        });
+        // test.only("Page (default ) playwright test", async ({ page }) => {
+
+        //   page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+        // });
