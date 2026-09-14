1da4da85d5013920998254debcb401eff624eca0
SuhaibRashad <mdsuhaibengineer@gmail.com>
2026-09-14 10:03:20 +0530

    L16 : extracting and validating text

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,15 +1,21 @@
-const {test} = require("@playwright/test")
+const {test, expect} = require("@playwright/test")
 
-    test.only(
+    test(
         "Browser Context playwright test",
         async ({browser}) => {
             const context = await browser.newContext();
             const page = await context.newPage();
             await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+            // css selector
             await page.locator("#username").fill("rahulshetty"); // id selector
             await page.locator("[type='password']").fill("learning"); // attribute selector
             await page.locator("#signInBtn").click()
 
+            // auto wait feature by playwirght for dynamic element appearence
+            console.log(await page.locator("[style*='block']").textContent());
+            // assertions to validate the extracted text
+            await expect(page.locator("[style*='block']")).toContainText('InCorrect');
 
         }
     )
